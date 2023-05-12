0c1b1b33d96f8c5958bb1a4788b61c9f7c8ed07a
AnzathuPindani <anzathu.pindani@nicotechnologies.com>
2023-05-12 06:46:36 +0200

    added get and post api

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 import 'package:shopinglis_app/data/categories.dart';
 import 'package:shopinglis_app/models/category.dart';
 import 'package:shopinglis_app/models/grocery_item.dart';
@@ -17,14 +21,22 @@ class _NewItemState extends State<NewItem> {
   int _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory));
+      final url = Uri.https('flutter-prep-e7495-default-rtdb.firebaseio.com',
+          'shopping-list.json');
+      final response = await http.post(url,
+          headers: {'Content-Type': 'application/json'},
+          body: json.encode({
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          }));
+      print(response.body);
+      if (mounted) {
+        Navigator.of(context).pop();
+      }
     }
   }
 
